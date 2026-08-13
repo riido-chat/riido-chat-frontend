2539815923d40947c0194281ead2230abf43e37d
LeeYunHwan <cyzhqly@gmail.com>
2026-08-13 20:33:35 +0900

    setting: Prettier 설정 및 ESLint 연동 #4

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,10 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
+import eslintConfigPrettier from 'eslint-config-prettier/flat';
+import { defineConfig, globalIgnores } from 'eslint/config';
 
 export default defineConfig([
   globalIgnores(['dist']),
@@ -19,4 +20,6 @@ export default defineConfig([
       globals: globals.browser,
     },
   },
-])
+
+  eslintConfigPrettier,
+]);
