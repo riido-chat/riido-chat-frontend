a12e9ebed762249e39fe28719db20a94508d9d5e
LeeYunHwan <cyzhqly@gmail.com>
2026-08-21 16:02:40 +0900

    fix: cn 타이포그래피 병합 오류 수정 #10

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,23 @@
 import { type ClassValue, clsx } from 'clsx';
-import { twMerge } from 'tailwind-merge';
+import { extendTailwindMerge } from 'tailwind-merge';
+
+const customTwMerge = extendTailwindMerge({
+  extend: {
+    classGroups: {
+      'font-size': [
+        'text-title-1',
+        'text-title-2',
+        'text-heading',
+        'text-headline',
+        'text-body-1',
+        'text-body-2',
+        'text-label',
+        'text-caption',
+      ],
+    },
+  },
+});
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
+  return customTwMerge(clsx(inputs));
 }
