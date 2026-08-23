9fd881e0e713f27a057d9234eb061e26bdfc63e1
LeeYunHwan <cyzhqly@gmail.com>
2026-08-24 02:50:29 +0900

    design: Card에 디자인 토큰 적용 및 레이아웃 기본값 이관 #17

--- src/components/common/card.tsx
+++ src/components/common/card.tsx
@@ -12,7 +12,7 @@ function Card({
       data-slot="card"
       data-size={size}
       className={cn(
-        'group/card bg-background-elevated text-label-normal flex flex-col gap-(--card-spacing) overflow-hidden rounded-xl py-(--card-spacing) text-sm [--card-spacing:--spacing(4)] has-data-[slot=card-footer]:pb-0 has-data-[slot=card-header]:pt-0 has-[>img:first-child]:pt-0 data-[size=sm]:[--card-spacing:--spacing(3)] *:[img:first-child]:rounded-t-xl *:[img:last-child]:rounded-b-xl',
+        'group/card bg-background-secondary text-label-normal shadow-rc-shadow flex flex-col gap-(--card-spacing) overflow-hidden rounded-xl py-(--card-spacing) text-sm [--card-spacing:--spacing(4)] has-data-[slot=card-footer]:pb-0 has-data-[slot=card-header]:pt-0 has-[>img:first-child]:pt-0 data-[size=sm]:[--card-spacing:--spacing(3)] *:[img:first-child]:rounded-t-xl *:[img:last-child]:rounded-b-xl',
         className,
       )}
       {...props}
@@ -25,7 +25,7 @@ function CardHeader({ className, ...props }: React.ComponentProps<'div'>) {
     <div
       data-slot="card-header"
       className={cn(
-        'group/card-header border-b-line-normal @container/card-header grid auto-rows-min items-start gap-1 rounded-t-xl border-b p-(--card-spacing) has-data-[slot=card-action]:grid-cols-[1fr_auto] has-data-[slot=card-description]:grid-rows-[auto_auto]',
+        'group/card-header border-b-line-normal @container/card-header grid auto-rows-min grid-cols-[auto_1fr_auto] items-center gap-1 rounded-t-xl border-b p-(--card-spacing) has-data-[slot=card-action]:grid-cols-[1fr_auto] has-data-[slot=card-description]:grid-rows-[auto_auto]',
         className,
       )}
       {...props}
@@ -38,7 +38,7 @@ function CardTitle({ className, ...props }: React.ComponentProps<'div'>) {
     <div
       data-slot="card-title"
       className={cn(
-        'text-base leading-snug font-medium group-data-[size=sm]/card:text-sm',
+        'text-label-alternative text-center text-base leading-snug font-bold group-data-[size=sm]/card:text-sm',
         className,
       )}
       {...props}
@@ -50,7 +50,7 @@ function CardDescription({ className, ...props }: React.ComponentProps<'div'>) {
   return (
     <div
       data-slot="card-description"
-      className={cn('text-muted-foreground text-sm', className)}
+      className={cn('text-label-assistive text-sm', className)}
       {...props}
     />
   );
@@ -77,7 +77,7 @@ function CardFooter({ className, ...props }: React.ComponentProps<'div'>) {
     <div
       data-slot="card-footer"
       className={cn(
-        'bg-background-normal flex items-center rounded-t-[20px] p-(--card-spacing) shadow-[0_-2px_20px_0_rgba(153,161,175,0.10)]',
+        'bg-background-default shadow-rc-shadow flex items-center rounded-t-[20px] p-(--card-spacing)',
         className,
       )}
       {...props}
