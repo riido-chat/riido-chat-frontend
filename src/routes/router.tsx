b702e6fd8097e19e141ca9969cfc03e67920dcbf
LeeYunHwan <cyzhqly@gmail.com>
2026-09-17 21:10:52 +0900

    chore: 가이드 페이지 및 관련 라우팅 제거 #116

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -2,7 +2,6 @@ import { createBrowserRouter, Navigate } from 'react-router';
 import RootLayout from '@/routes/RootLayout';
 import ConsoleLayout from '@/routes/ConsoleLayout';
 import HomePage from '@/pages/HomePage';
-import GuidePage from '@/pages/GuidePage';
 import DocumentGroupListPage from '@/pages/console/DocumentGroupListPage';
 import DocumentGroupDetailPage from '@/pages/console/DocumentGroupDetailPage';
 
@@ -22,10 +21,7 @@ const adminRoutes = [
 const publicRoutes = [
   {
     element: <RootLayout />,
-    children: [
-      { index: true, element: <HomePage /> },
-      { path: 'guide/:sectionId', element: <GuidePage /> },
-    ],
+    children: [{ index: true, element: <HomePage /> }],
   },
 ];
 
